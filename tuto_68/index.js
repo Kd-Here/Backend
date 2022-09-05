efa605ab44b3c71e7d54736f182b1cc704094a81
Kd-Here <gamelover9899@gmail.com>
2022-09-05 23:59:18 +0530

    68

---
+++ tuto_68/index.js
@@ -0,0 +1,18 @@
+// Here we are learning how to import and export modules in Js
+
+console.log("This is index file where we are importing other modules");
+
+
+const a = require("./mod.js");
+// console.log(avera[2,5,6]);
+// This is will not work
+
+
+console.log("This is bocx of a ",a);  // This will show you [Function: avera]
+
+// for using the module simply use the identifier which you use here i.e use avera   module.exports = avera
+
+
+const avera = require('./mod.js');
+console.log(avera);
+console.log(avera([2,5,6]));
---
+++ tuto_68/mod.js
@@ -0,0 +1,23 @@
+//This is file where our module which to import is present
+
+console.log("This is modules files which is imported by index file");
+
+
+function avera(elemnts) {
+    let sum = 0;
+    elemnts.forEach(element => {
+        sum += element;
+    });
+    return sum/elemnts.length;
+}
+
+// This is statement to export the modules
+module.exports = avera;
+// this means module should export above function avera
+
+
+
+// if you wanted to create obj of moudle export
+module.exports = {
+    name : "kd"
+}
