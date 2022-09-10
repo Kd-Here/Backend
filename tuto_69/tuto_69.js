74dc869a527f5e6c3bdc981dd48513143ad35858
Kd-Here <gamelover9899@gmail.com>
2022-09-10 16:04:24 +0530

    Completed tuto-69

---
+++ tuto_69/tuto_69.js
@@ -0,0 +1,10 @@
+// We have installed nodemon in tuto_69 folder check package json file all list of installed dependency with version is shown 
+
+
+// We are using npm init command which make npm json file storing all details of package versions.
+
+
+//Since this dependecies depend on other dependency installing one download other and cycle goes on so best practisce is use devdependecy check it
+
+
+//  All nodemouldes are include in gitignore folder
