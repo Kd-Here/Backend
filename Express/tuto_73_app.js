6712a1c0b9519b986fc94de3dae19bdf85753730
Kd-Here <gamelover9899@gmail.com>
2022-09-17 11:42:55 +0530

    Done with 74

--- Express/tuto_73_app.js
+++ Express/tuto_73_app.js
@@ -1,13 +1,20 @@
-const express = require("express")
+const express = require("express");
+const { fstat } = require("fs");
 const path = require('path')
 const app = express();
 const port = 80;
+const fs = require('fs');
+// Node.js as a File Server fs
+
 
 
 
 // Express setup
 
 app.use('/static',express.static('static'))  //For serving staic files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// This bcoz our url is encoded for what you type 
 
 
 // PUG releated files
@@ -25,6 +32,30 @@ app.get('/',(req,res)=>{
     const params ={ 'titile':'PugB is best game','info': con}
 
     res.status(200).render('index.pug',params)
+})
+
+
+
+app.post('/',(req,res)=>{
+
+    // Req from form with their name not with id
+    let Dict = {
+
+        "name" : req.body.name,
+        "Age" : req.body.age,
+        "Gen" : req.body.gender,
+        "Adrres" : req.body.address,
+        "Aboit" :req.body.more
+    };
+
+    ass = JSON.stringify(Dict);
+    // JSON.stringify(Dict) if not used it we simply displaying Dict gives object [object] means object is displaying another object
+    // We hd displayed data to user schareen we can also that in new file
+    let RegisteredMemberDetails = `Name : ${Dict["name"]} \n ${Dict["name"]} age is ${Dict["Age"]} who will workout in ${Dict["Gen"]} section who reside in ${Dict["Adrres"]} focusing to improve ${Dict["Aboit"]}`;
+    fs.writeFileSync('GymMemberRegister.txt',RegisteredMemberDetails)
+    sending ={'titile':'Access via post','info':ass}
+    console.log(Dict);
+    res.status(200).render('index.pug',sending)
 })
 
 //Start the server
