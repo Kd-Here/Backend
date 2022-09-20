606f10c01955f03cba56a1bb0cb95f17d203d55a
Kd-Here <gamelover9899@gmail.com>
2022-09-20 11:47:02 +0530

    Completed 76 Happy to do

---
+++ Tuto_75/static/index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    document.getElementById("sec").style.backgroundColor = "yellow";
+    
+}
+function myFunction1(){
+    document.getElementById("fir").style.backgroundColor = "green";
+    
+}
